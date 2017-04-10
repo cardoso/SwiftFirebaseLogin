85579b16e769932bb3254cc1be077b3835fbad54
Matheus Martins <matheusmcrds@gmail.com>
2017-04-10 10:55:48 -0300

    Added cocoapods, firebase, .gitignore

---
+++ SwiftFirebaseLogin/Pods/Target Support Files/Pods-SwiftFirebaseLogin/Pods-SwiftFirebaseLogin-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SwiftFirebaseLoginVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftFirebaseLoginVersionString[];
+
